e1a06bb3d6fc39f3df596ab2d298bba768dba6d6
parmeetk2005 <parmeetk2005@gmail.com>
2025-10-02 15:07:34 +0530

    Leetcode Questions

--- 2. Add two numbers/solution.java
+++ 2. Add two numbers/solution.java
@@ -1,14 +1,15 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
  */
-class Solution {
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode tail = dummyHead;
