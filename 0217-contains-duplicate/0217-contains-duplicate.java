953c10bf8dbd893923de3243ac4696ab3a0e8a89
parmeetk2005 <157354062+parmeetk2005@users.noreply.github.com>
2026-02-09 10:20:05 +0530

    Time: 14 ms (85.78%), Space: 93.5 MB (56.34%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,29 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        HashSet<Integer> st = new HashSet<>();   // HashSet is empty
+        HashSet<Integer> hs = new HashSet<>();
         for(int i = 0; i < nums.length; i++){
-            if(st.contains(nums[i])){     // if HashSet has elements then check
+            if(hs.contains(nums[i])){
                 return true;
             }
-            st.add(nums[i]);     // if HashSet is empty push the number we seen
+            hs.add(nums[i]);
         }
         return false;
     }
 }
-
-
-// TIME LIMIT EXCEED
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         Arrays.sort(nums);
-//         Stack<Integer> st = new Stack<>();  // stack is empty
-//         for(int i = 0; i < nums.length; i++){
-//             if(st.contains(nums[i])){    // if stack has elements then check 
-//                 return true;
-//             }
-//             st.push(nums[i]); // if stack is empty push the number we seen
-//         }
-//         return false;
-//     }
-// }
